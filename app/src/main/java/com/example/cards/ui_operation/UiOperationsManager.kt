11a1663a9cebfb33259ff6feb56b90b0a2e5dc2a
李宜雯 <liy721511@gmail.com>
2025-11-18 20:10:57 +0800

    (feat) 將需要判斷圖片的 function 改成讀取 getImageView

--- app/src/main/java/com/example/cards/ui_operation/UiOperationsManager.kt
+++ app/src/main/java/com/example/cards/ui_operation/UiOperationsManager.kt
@@ -15,44 +15,8 @@ class UiOperationsManager(private val activity:AppCompatActivity,private val bin
 
 
     fun setCarClickListener(imageName:String,listener:() -> Unit){
-        when(imageName){
-            "imageViewOne" ->{
-                binding.imageViewOne.setOnClickListener { listener.invoke() }
-            }
-            "imageViewTwo" ->{
-                binding.imageViewTwo.setOnClickListener {  listener.invoke() }
-            }
-            "imageViewThree" ->{
-                binding.imageViewThree.setOnClickListener {  listener.invoke()  }
-            }
-            "imageViewFour"->{
-                binding.imageViewFour.setOnClickListener {  listener.invoke()  }
-            }
-            "imageViewFive"->{
-                binding.imageViewFive.setOnClickListener { listener.invoke() }
-            }
-            "imageViewSix"->{
-                binding.imageViewSix.setOnClickListener {  listener.invoke()  }
-            }
-            "imageViewSeven"->{
-                binding.imageViewSeven.setOnClickListener {  listener.invoke()  }
-            }
-            "imageViewEight"->{
-                binding.imageViewEight.setOnClickListener { listener.invoke() }
-            }
-            "imageViewNine"->{
-                binding.imageViewNine.setOnClickListener {  listener.invoke()  }
-            }
-            "imageViewTen"->{
-                binding.imageViewTen.setOnClickListener {  listener.invoke() }
-            }
-            "imageViewEleven"->{
-                binding.imageViewEleven.setOnClickListener { listener.invoke() }
-            }
-            "imageViewTwelve"->{
-                binding.imageViewTwelve.setOnClickListener {  listener.invoke()  }
-            }
-        }
+        val imageView = getImageView(imageName)
+        imageView.setOnClickListener { listener.invoke() }
     }
 
     fun set_mycard_Image(imageName:String,number:Int){
@@ -70,173 +34,27 @@ class UiOperationsManager(private val activity:AppCompatActivity,private val bin
     }
 
     fun setImageIsEnabled(imageName:String , isEnabled:Boolean){
-
-        when(imageName){
-             "imageViewOne" ->{
-                 binding.imageViewOne.isEnabled = isEnabled
-             }
-             "imageViewTwo" ->{
-                 binding.imageViewTwo.isEnabled = isEnabled
-             }
-             "imageViewThree" ->{
-                 binding.imageViewThree.isEnabled = isEnabled
-             }
-             "imageViewFour"->{
-                 binding.imageViewFour.isEnabled = isEnabled
-             }
-             "imageViewFive"->{
-                 binding.imageViewFive.isEnabled = isEnabled
-             }
-             "imageViewSix"->{
-                 binding.imageViewSix.isEnabled = isEnabled
-             }
-             "imageViewSeven"->{
-                 binding.imageViewSeven.isEnabled = isEnabled
-             }
-             "imageViewEight"->{
-                 binding.imageViewEight.isEnabled = isEnabled
-             }
-             "imageViewNine"->{
-                 binding.imageViewNine.isEnabled = isEnabled
-             }
-             "imageViewTen"->{
-                 binding.imageViewTen.isEnabled = isEnabled
-             }
-             "imageViewEleven"->{
-                 binding.imageViewEleven.isEnabled = isEnabled
-             }
-             "imageViewTwelve"->{
-                 binding.imageViewTwelve.isEnabled = isEnabled
-             }
-        }
-
+        val imageView = getImageView(imageName)
+        imageView.isEnabled = isEnabled
     }
     fun performDisappearAnimation(imageName: String,animationEndListener:AnimationEndListener){
 
         val my_animation = AnimationUtils.loadAnimation(activity,R.anim.disappear)
-        when(imageName){
-            "imageViewOne" ->{
-                _performAnimation(binding.imageViewOne,animationEndListener,my_animation)
-            }
-            "imageViewTwo" ->{
-                _performAnimation( binding.imageViewTwo,animationEndListener,my_animation)
-            }
-            "imageViewThree" ->{
-                _performAnimation(binding.imageViewThree,animationEndListener,my_animation)
-            }
-            "imageViewFour"->{
-                _performAnimation(binding.imageViewFour,animationEndListener,my_animation)
-            }
-            "imageViewFive"->{
-                _performAnimation( binding.imageViewFive,animationEndListener,my_animation)
-            }
-            "imageViewSix"->{
-                _performAnimation(binding.imageViewSix,animationEndListener,my_animation)
-            }
-            "imageViewSeven"->{
-                _performAnimation(binding.imageViewSeven,animationEndListener,my_animation)
-            }
-            "imageViewEight"->{
-                _performAnimation(binding.imageViewEight,animationEndListener,my_animation)
-            }
-            "imageViewNine"->{
-                _performAnimation(binding.imageViewNine,animationEndListener,my_animation)
-            }
-            "imageViewTen"->{
-                _performAnimation(binding.imageViewTen,animationEndListener,my_animation)
-            }
-            "imageViewEleven"->{
-                _performAnimation(binding.imageViewEleven,animationEndListener,my_animation)
-            }
-            "imageViewTwelve"->{
-                _performAnimation(binding.imageViewTwelve,animationEndListener,my_animation)
-            }
-        }
+        val imageView = getImageView(imageName)
+        _performAnimation(imageView,animationEndListener,my_animation)
     }
     fun performShackAnimation(imageName: String,animationEndListener:AnimationEndListener){
 
         val my_animation = AnimationUtils.loadAnimation(activity,R.anim.shake)
-        when(imageName){
-            "imageViewOne" ->{
-                _performAnimation(binding.imageViewOne,animationEndListener,my_animation)
-            }
-            "imageViewTwo" ->{
-                _performAnimation( binding.imageViewTwo,animationEndListener,my_animation)
-            }
-            "imageViewThree" ->{
-                _performAnimation(binding.imageViewThree,animationEndListener,my_animation)
-            }
-            "imageViewFour"->{
-                _performAnimation(binding.imageViewFour,animationEndListener,my_animation)
-            }
-            "imageViewFive"->{
-                _performAnimation( binding.imageViewFive,animationEndListener,my_animation)
-            }
-            "imageViewSix"->{
-                _performAnimation(binding.imageViewSix,animationEndListener,my_animation)
-            }
-            "imageViewSeven"->{
-                _performAnimation(binding.imageViewSeven,animationEndListener,my_animation)
-            }
-            "imageViewEight"->{
-                _performAnimation(binding.imageViewEight,animationEndListener,my_animation)
-            }
-            "imageViewNine"->{
-                _performAnimation(binding.imageViewNine,animationEndListener,my_animation)
-            }
-            "imageViewTen"->{
-                _performAnimation(binding.imageViewTen,animationEndListener,my_animation)
-            }
-            "imageViewEleven"->{
-                _performAnimation(binding.imageViewEleven,animationEndListener,my_animation)
-            }
-            "imageViewTwelve"->{
-                _performAnimation(binding.imageViewTwelve,animationEndListener,my_animation)
-            }
-        }
+        val imageView = getImageView(imageName)
+        _performAnimation(imageView,animationEndListener,my_animation)
     }
 
     fun performBackAnimation(imageName: String,animationEndListener:AnimationEndListener){
 
         val my_animation =  AnimationUtils.loadAnimation(activity, R.anim.back)
-        when(imageName){
-            "imageViewOne" ->{
-                _performAnimation(binding.imageViewOne,animationEndListener,my_animation)
-            }
-            "imageViewTwo" ->{
-                _performAnimation( binding.imageViewTwo,animationEndListener,my_animation)
-            }
-            "imageViewThree" ->{
-                _performAnimation(binding.imageViewThree,animationEndListener,my_animation)
-            }
-            "imageViewFour"->{
-                _performAnimation(binding.imageViewFour,animationEndListener,my_animation)
-            }
-            "imageViewFive"->{
-                _performAnimation( binding.imageViewFive,animationEndListener,my_animation)
-            }
-            "imageViewSix"->{
-                _performAnimation(binding.imageViewSix,animationEndListener,my_animation)
-            }
-            "imageViewSeven"->{
-                _performAnimation(binding.imageViewSeven,animationEndListener,my_animation)
-            }
-            "imageViewEight"->{
-                _performAnimation(binding.imageViewEight,animationEndListener,my_animation)
-            }
-            "imageViewNine"->{
-                _performAnimation(binding.imageViewNine,animationEndListener,my_animation)
-            }
-            "imageViewTen"->{
-                _performAnimation(binding.imageViewTen,animationEndListener,my_animation)
-            }
-            "imageViewEleven"->{
-                _performAnimation(binding.imageViewEleven,animationEndListener,my_animation)
-            }
-            "imageViewTwelve"->{
-                _performAnimation(binding.imageViewTwelve,animationEndListener,my_animation)
-            }
-        }
+        val imageView = getImageView(imageName)
+        _performAnimation(imageView,animationEndListener,my_animation)
     }
 
    private fun _performAnimation(imageView:ImageView,animationEndListener:AnimationEndListener,animation: Animation){
@@ -269,44 +87,8 @@ class UiOperationsManager(private val activity:AppCompatActivity,private val bin
 
     fun performFrontAnimation(imageName:String){
         var my_animation = AnimationUtils.loadAnimation(activity, R.anim.font)
-        when(imageName){
-            "imageViewOne" ->{
-                binding.imageViewOne.startAnimation(my_animation)
-            }
-            "imageViewTwo" ->{
-                binding.imageViewTwo.startAnimation(my_animation)
-            }
-            "imageViewThree" ->{
-                binding.imageViewThree.startAnimation(my_animation)
-            }
-            "imageViewFour"->{
-                binding.imageViewFour.startAnimation(my_animation)
-            }
-            "imageViewFive"->{
-                binding.imageViewFive.startAnimation(my_animation)
-            }
-            "imageViewSix"->{
-                binding.imageViewSix.startAnimation(my_animation)
-            }
-            "imageViewSeven"->{
-                binding.imageViewSeven.startAnimation(my_animation)
-            }
-            "imageViewEight"->{
-                binding.imageViewEight.startAnimation(my_animation)
-            }
-            "imageViewNine"->{
-                binding.imageViewNine.startAnimation(my_animation)
-            }
-            "imageViewTen"->{
-                binding.imageViewTen.startAnimation(my_animation)
-            }
-            "imageViewEleven"->{
-                binding.imageViewEleven.startAnimation(my_animation)
-            }
-            "imageViewTwelve"->{
-                binding.imageViewTwelve.startAnimation(my_animation)
-            }
-        }
+        val imageView = getImageView(imageName)
+        imageView.startAnimation(my_animation)
     }
     fun getDrawable(number: Int):Int{
         when(number){
